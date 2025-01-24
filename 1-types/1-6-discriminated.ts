4541f9bf79a37a487d90ca007fda5e5be9c37c2d
jisoolee <szjslee@gmail.com>
2025-01-24 23:55:36 +0900

    discriminated union

--- 1-types/1-6-discriminated.ts
+++ 1-types/1-6-discriminated.ts
@@ -1,22 +1,23 @@
 {
   // function: login -> success, fail ⏱
+  // 여러 타입 중 하나만 가질 수 있는 객체 정의 시 사용
   type SuccessState = {
-    result: 'success';
+    result: "success"; // 공통 속성
     response: {
       body: string;
     };
   };
   type FailState = {
-    result: 'fail';
+    result: "fail"; // 공통 속성
     reason: string;
   };
-  type LoginState = SuccessState | FailState;
+  type LoginState = SuccessState | FailState; // discriminated union
 
   function login(): LoginState {
     return {
-      result: 'success',
+      result: "success",
       response: {
-        body: 'logged in!',
+        body: "logged in!",
       },
     };
   }
@@ -25,7 +26,7 @@
   // success -> 🎉 body
   // fail -> 😭 reason
   function printLoginState(state: LoginState) {
-    if (state.result === 'success') {
+    if (state.result === "success") {
       console.log(`🎉 ${state.response.body}`);
     } else {
       console.log(`😭 ${state.reason}`);
