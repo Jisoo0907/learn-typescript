c460d87b6827c9a36306444d87b321c712f9a3c7
jisoolee <szjslee@gmail.com>
2025-01-25 19:50:47 +0900

    intersection types

--- 1-types/1-7-intersection.ts
+++ 1-types/1-7-intersection.ts
@@ -13,11 +13,14 @@
   };
 
   function internWork(person: Student & Worker) {
+    // 타입 결합
     console.log(person.name, person.empolyeeId, person.work());
   }
 
+  // 교차 타입 선언 시 두 타입 모두 사용해야 함
+  // 하나라도 포함 않할 시 에러
   internWork({
-    name: 'ellie',
+    name: "ellie",
     score: 1,
     empolyeeId: 123,
     work: () => {},
