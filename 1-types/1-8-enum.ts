d23673f5dd573c8aa4ac5a3a7ff41a90ebff95cd
jisoolee <szjslee@gmail.com>
2025-01-25 20:26:42 +0900

    enum

--- 1-types/1-8-enum.ts
+++ 1-types/1-8-enum.ts
@@ -12,22 +12,23 @@
   const dayOfToday = DAYS_ENUM.MONDAY;
 
   // TypeScript
-  type DaysOfWeek = 'Monday' | 'Tuesday' | 'Wednesday';
+  type DaysOfWeek = "Monday" | "Tuesday" | "Wednesday";
   enum Days {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday,
+    Monday, // 0
+    Tuesday, // 1
+    Wednesday, // 2
+    Thursday, // 3
+    Friday, // 4
+    Saturday, // 5
+    Sunday, // 6
   }
   console.log(Days.Monday);
   let day: Days = Days.Saturday;
   day = Days.Tuesday;
-  day = 10;
+  // day = 10; Error - 정의된 값 0~6만 할당 가능
+  day = 1;
   console.log(day);
 
-  let dayOfweek: DaysOfWeek = 'Monday';
-  dayOfweek = 'Wednesday';
+  let dayOfweek: DaysOfWeek = "Monday";
+  dayOfweek = "Wednesday";
 }
