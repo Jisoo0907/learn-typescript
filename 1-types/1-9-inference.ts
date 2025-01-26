b9669eb46b5dbd5740d095398f2bf8d3c3bd1cc6
jisoolee <szjslee@gmail.com>
2025-01-26 23:51:49 +0900

    type inference

--- 1-types/1-9-inference.ts
+++ 1-types/1-9-inference.ts
@@ -1,13 +1,15 @@
 /**
  * Type Inference
  */
-let text = 'hello';
-function print(message = 'hello') {
+let text = "hello";
+function print(message = "hello") {
+  // 따로 타입 명시하지 않으면 any
+  // default parameter
   console.log(message);
 }
-print('hello');
+print("hello");
 
 function add(x: number, y: number): number {
   return x + y;
 }
-const result = add(1, 2);
+const result = add(1, 2); // result는 자동으로 숫자 타입
