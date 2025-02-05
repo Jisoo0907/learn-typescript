4ad2173809b350a40797a06a73fe06693cba6188
jisoolee <szjslee@gmail.com>
2025-02-05 22:30:05 +0900

    make coffee machine w.procedural programming

---
+++ 3-types-oop/3-1-without-oop.ts
@@ -0,0 +1,24 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  const BEANS_GRAMM_PER_SHOT: number = 7; // number 생략 가능
+  let coffeeBeans: number = 0;
+
+  function makeCoffee(shots: number): CoffeeCup {
+    if (coffeeBeans < shots * BEANS_GRAMM_PER_SHOT) {
+      throw new Error("Not enough coffee beans!");
+    }
+    coffeeBeans == shots * BEANS_GRAMM_PER_SHOT; // 사용한 만큼 감소
+    return {
+      shots, // shots: shots, key와 value 이름 동일 시 생략 가능
+      hasMilk: false,
+    };
+  }
+
+  coffeeBeans += 3 * BEANS_GRAMM_PER_SHOT;
+  const coffee = makeCoffee(2);
+  console.log(coffee);
+}
