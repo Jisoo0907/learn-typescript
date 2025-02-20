ff6eec1aeb1f9b75750ee2fd512ebdba3640c925
jisoolee <szjslee@gmail.com>
2025-02-20 20:36:32 +0900

    implement stack

--- 4-oop-project/stack.ts
+++ 4-oop-project/stack.ts
@@ -3,3 +3,46 @@ interface Stack {
   push(value: string): void;
   pop(value: string): void;
 }
+
+type StackNode = {
+  readonly value: string;
+  readonly next?: StackNode;
+};
+
+class StackImpl implements Stack {
+  private _size: number = 0;
+  private head?: StackNode;
+
+  constructor(private capacity: number) {}
+
+  get size() {
+    return this._size;
+  }
+
+  push(value: string) {
+    if (this.size === this.capacity) {
+      throw new Error("Stack is full!");
+    }
+    const node: StackNode = { value, next: this.head };
+    this.head = node;
+    this._size++;
+  }
+
+  pop(): string {
+    if (this.head == null) {
+      throw new Error("Stack is empty!");
+    }
+    const node = this.head;
+    this.head = node.next;
+    this._size--;
+    return node.value;
+  }
+}
+
+const stack = new StackImpl(10);
+stack.push("Kelly 1");
+stack.push("Bob 2");
+stack.push("Joanna 3");
+while (stack.size !== 0) {
+  console.log(stack.pop());
+}
